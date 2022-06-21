1973b9f8528c3af79d18fdaed4f1b9dc3869d587
Jonathan Gregorsky <jonathan.gregorsky@outlook.com>
2022-06-21 12:50:40 -0400

    🚧 🐛 Add converter for DateTimes

--- src/mmmPizza.MarvelSdk/MarvelApiService.cs
+++ src/mmmPizza.MarvelSdk/MarvelApiService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
 using Flurl;
 using Flurl.Http;
 
@@ -7,29 +8,30 @@ namespace mmmPizza.MarvelSdk;
 
 public class MarvelApiService
 {
-    // Constructor
-    private MarvelApiService() { }
-
-
     // Private Properties
-    private string _privateApiKey = default!;
-    private string _publicApiKey = default!;
+    private readonly JsonSerializerOptions _jsonOptions = default!;
+    private readonly string _privateApiKey = default!;
+    private readonly string _publicApiKey = default!;
 
     private long _currentTimeStamp;
 
 
-    public static MarvelApiService Create(string privateApiKey, string publicApiKey)
+    // Constructors
+    private MarvelApiService() { }
+    public MarvelApiService(string privateApiKey, string publicApiKey)
     {
         ArgumentNullException.ThrowIfNull(privateApiKey);
         ArgumentNullException.ThrowIfNull(publicApiKey);
 
-        return new MarvelApiService
-        {
-            _privateApiKey = privateApiKey,
-            _publicApiKey = publicApiKey
-        };
-    }
+        _privateApiKey = privateApiKey;
+        _publicApiKey = publicApiKey;
 
+        _jsonOptions = new()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+        _jsonOptions.Converters.Add(new MarvelDateTimeConverter("yyyy-MM-ddTHH:mm:ssK"));
+    }
 
 
     public async Task<DataWrapper<Comic>> GetComics()
@@ -42,14 +44,18 @@ public class MarvelApiService
             .SetQueryParam("ts", _currentTimeStamp)
             .SetQueryParam("apikey", _publicApiKey)
             .SetQueryParam("hash", GetHash());
-        
-        var response = await comicsUrl.GetAsync();
-        if (response.StatusCode != 200) throw new Exception();
-        
-        string json = await response.GetJsonAsync();
-        json = json.Replace("-0001-11-30T00:00:00-0500", "");
 
-        DataWrapper<Comic> comics = System.Text.Json.JsonSerializer.Deserialize<DataWrapper<Comic>>(json);
+        var response = comicsUrl
+            .GetAsync()
+            .Result
+            .ThrowOnError();
+
+        if (response.StatusCode != 200) throw new Exception();
+
+        string json = await response.GetStringAsync();
+        json = json.Replace("-0001-11-30T00:00:00-0500", "1900-01-01T00:00:00-00:00");
+
+        DataWrapper<Comic> comics = JsonSerializer.Deserialize<DataWrapper<Comic>>(json, _jsonOptions);
 
         return comics;
     }
@@ -74,7 +80,7 @@ public class MarvelApiService
             hashBytes = md5
                 .ComputeHash(Encoding.UTF8.GetBytes($"{_currentTimeStamp}{_privateApiKey}{_publicApiKey}"));
         }
-        
+
         return BitConverter.ToString(hashBytes).Replace("-", string.Empty).ToLower();
     }
 }
