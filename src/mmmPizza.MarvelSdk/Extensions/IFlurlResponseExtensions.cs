c802eb2b7862befb9b8e5a3679bc6d5bf3d6a15a
Jonathan Gregorsky <jonathan.gregorsky@outlook.com>
2022-06-21 09:57:48 -0400

    💡 Comment a TODO

--- src/mmmPizza.MarvelSdk/Extensions/IFlurlResponseExtensions.cs
+++ src/mmmPizza.MarvelSdk/Extensions/IFlurlResponseExtensions.cs
@@ -6,6 +6,8 @@ public static class IFlurlResponseExtensions
 {
     public static IFlurlResponse ThrowOnError(this IFlurlResponse response)
     {
+        // TODO: Implement API Error handling
+        // https://developer.marvel.com/documentation/authorization
         return response;
     }
 }
