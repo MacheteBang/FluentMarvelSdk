1942930314c9be15ae8d9ce44290479339dcdc29
Jonathan Gregorsky <jonathan.gregorsky@outlook.com>
2022-06-22 11:53:08 -0400

    🏷️ Init

---
+++ src/mmmPizza.MarvelSdk/Enums/ComicFormatTypes.cs
@@ -0,0 +1,9 @@
+using Ardalis.SmartEnum;
+
+public sealed class ComicFormatTypes : SmartEnum<ComicFormatTypes, string>
+{
+    public static readonly ComicFormatTypes Comic = new ComicFormatTypes(nameof(Comic), "comic");
+    public static readonly ComicFormatTypes Collection = new ComicFormatTypes(nameof(Collection), "collection");
+
+    private ComicFormatTypes(string name, string value) : base (name, value) {}
+}
