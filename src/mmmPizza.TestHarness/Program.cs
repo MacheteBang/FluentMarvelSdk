0a42cec6c945bba53e10c99910b14c856f3c147f
Jonathan Gregorsky <jonathan.gregorsky@outlook.com>
2022-06-23 13:54:54 -0400

    ✅ Clean harness

--- src/mmmPizza.TestHarness/Program.cs
+++ src/mmmPizza.TestHarness/Program.cs
@@ -12,22 +12,3 @@ AnsiConsole.MarkupLine("Creating instance...");
 var api = new MarvelApiService(_configuration["Marvel:PrivateKey"], _configuration["Marvel:PublicKey"]);
 
 AnsiConsole.MarkupLine("Getting all comics...");
-var series = await api.GetSeriesAsync(new SeriesOptionSet
-{
-    TitleStartsWith = "The Amazing Spider-Man",
-    SeriesType = SeriesTypes.Ongoing
-});
-
-foreach (var x in series.Results)
-{
-    Console.WriteLine($"{x.Id} {x.Title}");
-}
-
-
-var asm = await api.GetComicsAsync(new ComicOptionSet
-{
-    IssueNumber = 2,
-    Series = new int[] { 32866 }
-});
-Console.WriteLine();
-
