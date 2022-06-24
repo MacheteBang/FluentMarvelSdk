6f9c2e12eef86d7fcee1b9672bd8ef3fc9cbd463
Jonathan Gregorsky <jonathan.gregorsky@outlook.com>
2022-06-24 15:08:24 -0400

    🥅 Report on Empty Parameter

--- src/FluentMarvelSdk/Extensions/IFlurlResponseExtensions.cs
+++ src/FluentMarvelSdk/Extensions/IFlurlResponseExtensions.cs
@@ -7,7 +7,7 @@ public static class IFlurlResponseExtensions
     public static IFlurlResponse ThrowOnError(this IFlurlResponse response)
     {
         if (response.StatusCode < 400) return response;
-        
+
         ApiError error = response.GetJsonAsync<ApiError>().Result;
 
         if (response.StatusCode >= 500) throw new MarvelApiException(error);
@@ -18,6 +18,7 @@ public static class IFlurlResponseExtensions
                 if (error.Code == "InvalidCredentials") throw new InvalidLimitException(error);
                 break;
             case 409:
+                if (error.Status.Contains("cannot be blank if it is set")) throw new EmptyParameterException(error);
                 if (error.Status.Contains("We don't recognize the parameter")) throw new InvalidParameterException(error);
                 if (error.Status.Contains("may not request more than")) throw new InvalidLimitException(error);
                 if (error.Status.Contains("limit greater than")) throw new InvalidLimitException(error);
